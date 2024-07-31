95006c603bdb40fe87eada31fd8c6a553bf30f48
Võ Quốc Việt <119559241+VoViet266@users.noreply.github.com>
2024-07-31 15:57:20 +0700

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,128 +1,147 @@
-import React, { useState, useEffect } from "react";
-import {
-  DownOutlined,
-  Html5Outlined,
-  ArrowDownOutlined,
-  MenuOutlined,
-  CloseOutlined,
-} from "@ant-design/icons";
-import { Button, Dropdown, Space } from "antd";
-import "./header.scss";
+import React, { useState } from "react";
+import { UploadOutlined, DeleteOutlined, CaretDownFilled } from "@ant-design/icons";
+import { Avatar, Button, Input, Typography, Select, Upload } from "antd";
+import "./setting.scss";
 
-const Header = () => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 835);
-  const [isOpen, setIsOpen] = useState(false);
-  const handleClick = () => {
-    setIsOpen(!isOpen);
-  };
-  useEffect(() => {
-    const handleResize = () => {
-      const isNowMobile = window.innerWidth <= 835;
-      setIsMobile(isNowMobile);
-      if (!isNowMobile) {
-        setIsOpen(false);
-      }
-    };
-    window.addEventListener("resize", handleResize);
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-  // Menu items for the first dropdown
-  const itemsPlugins = [
-    { key: "1", label: <a href="#">Plugin 1</a> },
-    { key: "2", label: <a href="#">Plugin 2</a> },
-    { key: "3", label: <a href="#">Plugin 3</a> },
-  ];
-
-  // Menu items for the second dropdown
-  const menuNav = [
-    { key: "1", label: <a href="#home">Home</a> },
-    { key: "2", label: <a href="#">Conversations</a> },
-    {
-      key: "3",
-      label: <a href="#">Plugins</a>,
-      children: [
-        { key: "3.1", label: <a href="#">Plugin 1</a> },
-        { key: "3.2", label: <a href="#">Plugin 2</a> },
-        { key: "3.3", label: <a href="#">Plugin 3</a> },
-      ],
+const Setting = () => {
+  const [mobileUserId, setMobileUserId] = useState(""); // State để lưu giá trị ID tài khoản di động
+  const props = {
+    name: "file",
+    //Đổi action
+    action: "",
+    headers: {
+      authorization: "authorization-text",
     },
-    { key: "4", label: <a href="#">Pricing</a> },
-    { key: "5", label: <a href="">Download</a> },
-    { key: "6", label: <a href="">Api</a> },
-  ];
-  // Menu items for the language dropdown
-  const itemsLanguage = [
-    { key: "1", label: <a href="#">EN</a> },
-    { key: "2", label: <a href="#">FR</a> },
-    { key: "3", label: <a href="#">ES</a> },
-    { key: "4", label: <a href="#">DE</a> },
-    { key: "5", label: <a href="#">IT</a> },
-    { key: "6", label: <a href="#">VN</a> },
-    { key: "7", label: <a href="#">JP</a> },
-    { key: "8", label: <a href="#">CN</a> },
-  ];
+  };
+
+  const handleInputChange = (e) => {
+    setMobileUserId(e.target.value); // Cập nhật state khi thay đổi giá trị input
+  };
 
   return (
-    <header>
-      <div className="logo">
-        <Html5Outlined />
+    <>
+      <div className="container">
+        <div className="container-item">
+          <div className="hearder-setting">
+            <h1 className="text-hearder">Settings</h1>
+            <p className="text-hearder">Manage and secure your account</p>
+          </div>
+          <div className="avatar">
+            <div>
+              <h3 className="text-bodyS">Profile picture</h3>
+              <p className="text-bodyS">Accepted files JPG, PNG, SVG</p>
+            </div>
+            <div className="btn">
+              {/* Dẫn giá trị từ backend vào src*/}
+              <Avatar size={64} src="" />
+              <Upload {...props}>
+                <Button icon={<UploadOutlined />}>Upload</Button>
+              </Upload>
+              <Button
+                className="ant-btn-delete"
+                danger
+                icon={<DeleteOutlined />}
+                size="large"
+              >
+                Delete
+              </Button>
+            </div>
+          </div>
+          <div className="personal-infor">
+            <div>
+              <h3 className="text-bodyS">Personal Information</h3>
+              <p className="text-bodyS">Shown on your public profile</p>
+            </div>
+            <div>
+              {/* Dẫn giá trị từ backend vào value*/}
+              <form className="form-input" action="" method="POST">
+                <div className="form-grid">
+                  <div className="form-item">
+                    <Typography.Title level={4}>First Name</Typography.Title>
+                    <Input  />
+                  </div>
+                  <div className="form-item">
+                    <Typography.Title level={4}>Last Name</Typography.Title>
+                    <Input />
+                  </div>
+                  <div className="form-item">
+                    <Typography.Title level={4}>Email Address</Typography.Title>
+                    {/* Dẫn giá trị từ backend vào value*/}
+                    <Input  value={`email`} />
+                  </div>
+                  <div className="form-item">
+                    <Typography.Title level={4}>Choose...</Typography.Title>
+                    <Select
+                      size={"large"}
+                      defaultValue=""
+                      style={{ width: 200 }}
+                      options={[
+                        { label: "Students", value: "a1"},
+                        { label: "Professionals", value: "a2" },
+                        { label: "Writer", value: "a3" },
+                        { label: "Educator", value: "a4" },
+                        { label: "Legal/Finance", value: "a5" },
+                      ]}
+                      suffixIcon={<CaretDownFilled />} 
+                    />
+                  </div>
+                </div>
+                <div className="container-button">
+                  <Button className="ant-btn-save" type="primary">
+                    Save
+                  </Button>
+                  <Button className="ant-btn-cancel">Cancel</Button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="password-item">
+            <div>
+              <h3 className="text-bodyS">Password</h3>
+              <p className="text-bodyS">Secure your account</p>
+            </div>
+            <Button type="primary">Reset Password</Button>
+          </div>
+          <div className="mobile-user">
+            <div>
+              <h3 className="text-bodyS">Link Your Mobile Account</h3>
+              <p className="text-bodyS">
+                Enter your mobile account ID to link your mobile account to your
+                web account
+              </p>
+            </div>
+            <form className="link-account" action="" method="POST">
+              <Typography.Title level={4}>Mobile User ID</Typography.Title>
+              <Input
+                value={mobileUserId}
+                onChange={handleInputChange}
+                placeholder="Enter ID"
+              />
+              <div className="container-button">
+                <Button
+                  className="ant-btn-save"
+                  type="primary"
+                  disabled={!mobileUserId}
+                >
+                  Link Account
+                </Button>
+              </div>
+            </form>
+          </div>
+          <div className="delete-account">
+            <div>
+              <h3 className="text-bodyS">Delete your account</h3>
+              <p className="text-bodyS">
+                Permanently remove all your data, including documents, chats,
+                and personal information.
+              </p>
+            </div>
+            <Button type="primary">Delete My Account</Button>
+          </div>
+        </div>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#">Conversations</a>
-          </li>
-          {/* Dropdown for plugins */}
-          <Dropdown menu={{ items: itemsPlugins }}>
-            <a onClick={(e) => e.preventDefault()}>
-              <Space>
-                Plugins
-                <DownOutlined />
-              </Space>
-            </a>
-          </Dropdown>
-          <li>
-            <a href="#">Pricing</a>
-          </li>
-          <li>
-            <a href="">Download</a>
-          </li>
-          <li>
-            <a href="">Api</a>
-          </li>
-        </ul>
-      </nav>
-      <div>
-        {/* Dropdown for language */}
-        <Dropdown menu={{ items: itemsLanguage }} trigger={["click"]}>
-          <Button>
-            <Space>
-              EN
-              <ArrowDownOutlined />
-            </Space>
-          </Button>
-        </Dropdown>
-        {isMobile && (
-          <Dropdown
-            className="menu-dropdown"
-            menu={{ items: menuNav }}
-            trigger={["click"]}
-            onClick={handleClick}
-            onOpenChange={(open) => setIsOpen(open)}
-          >
-            <a onClick={(e) => e.preventDefault()}>
-              <Space>{isOpen ? <CloseOutlined /> : <MenuOutlined />}</Space>
-            </a>
-          </Dropdown>
-        )}
-      </div>
-    </header>
+    </>
   );
 };
 
-export default Header;
+export default Setting;
